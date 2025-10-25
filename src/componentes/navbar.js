753df59bd4e1f5dd4c256e549e3361935d15e07a
alxexandracrispin <a.crispin@duocuc.cl>
2025-10-25 12:28:44 -0300

    primera prueba

---
+++ src/componentes/navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+
+
+function tituloPrincipal(){
+    console.log('Componente montado');
+
+    return (
+
+        <Navbar>
+            
+        </Navbar>
+    )
+}
